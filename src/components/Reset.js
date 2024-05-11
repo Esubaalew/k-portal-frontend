ad18fca3335a69ba9a4f98ac8b455dcf3b9486c1
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-05-12 01:44:12 +0300

    Revert "add reset fixc"
    This reverts commit 8b86d7ce575a6d5658129a4cacbea72f8ad7b556.

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { requestPasswordReset } from '../API/auth';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Reset.css';
 
 const Reset = () => {
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-  const [successMessage, setSuccessMessage] = useState('');
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
 
   const handleResetPassword = async () => {
     if (!email) {
@@ -17,12 +19,13 @@ const Reset = () => {
     try {
       setLoading(true); 
       await requestPasswordReset({ email });
-      setSuccessMessage('Password reset link has been sent to your email.');
+      console.log('Password reset request sent successfully.');
+      setSuccess(true);
+      // Optionally, you can automatically navigate the user to another page here
+      navigate('/');
     } catch (error) {
+      console.error('Error requesting password reset:', error);
       setError('Failed to send password reset request. Please try again.');
-      if (error.response && error.response.data && error.response.data.error) {
-        setError(error.response.data.error);
-      }
     } finally {
       setLoading(false);
     }
@@ -35,14 +38,17 @@ const Reset = () => {
   return (
     <div className="reset-container">
       <h2>Reset Password</h2>
-      <div className="reset-form">
-        <input type="email" name="email" placeholder="Email" onChange={handleChange} className="input-field" />
-        {error && <p className="error-message">{error}</p>}
-        {successMessage && <p className="success-message">{successMessage}</p>}
-        <button onClick={handleResetPassword} className="reset-button" disabled={loading}>
-          {loading ? <i className="fa fa-spinner fa-spin"></i> : 'Reset Password'}
-        </button>
-      </div>
+      {success ? (
+        <p className="success-message">Password reset link has been sent to your email.</p>
+      ) : (
+        <div className="reset-form">
+          <input type="email" name="email" placeholder="Email" onChange={handleChange} className="input-field" />
+          {error && <p className="error-message">{error}</p>}
+          <button onClick={handleResetPassword} className="reset-button" disabled={loading}>
+            {loading ? <i className="fa fa-spinner fa-spin"></i> : 'Reset Password'}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
